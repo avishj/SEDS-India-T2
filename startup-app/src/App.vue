38b8b583a86654313a10ac81b2fab1510218e4b2
avishj <avish.j@protonmail.com>
2020-08-07 14:38:27 +0530

    Finished Navbar

---
+++ startup-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  name: "App",
+  components: {
+    NavBar,
+    Footer
+  }
+};
+</script>
+
+<style>
+@import "./assets/css/lib/bootstrap.css";
+@import "./assets/css/lib/bootstrap-vue.css";
+</style>
